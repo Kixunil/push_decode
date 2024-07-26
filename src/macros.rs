8870344cb5ef91891936fbd476df233eb64c8584
Martin Habovstiak <martin.habovstiak@gmail.com>
2024-07-26 11:33:26 +0200

    Add `delegate!` macro
    Delegating the `Encoder` trait is pretty common but so far there wasn't
    a macro for it. This adds the macro that decreases the boilerplate a
    bit.

--- src/macros.rs
+++ src/macros.rs
@@ -20,3 +20,50 @@ macro_rules! mapped_decoder {
         )*
     }
 }
+
+#[macro_export]
+macro_rules! delegate {
+    (impl$(<$($gen:ident $(: $gen_bounds:path)?),*>)? Encoder for $ty:ty $(where $($where_ty:ty: $($where_bound:path)?),*)? { $field:tt }) => {
+        impl$(<$($gen $(: $gen_bounds)?)*>)? $crate::Encoder for $ty $(where $($where_ty: $($where_bound)?),*)? {
+            fn encoded_chunk(&self) -> &[u8] {
+                $crate::Encoder::encoded_chunk(&self.$field)
+            }
+
+            fn next(&mut self) -> bool {
+                $crate::Encoder::next(&mut self.$field)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    #[allow(unused)]
+    fn compiles() {
+        use crate::encoders::{ByteEncoder, BytesEncoder};
+        struct Foo(ByteEncoder);
+
+        delegate! {
+            impl Encoder for Foo { 0 }
+        };
+
+        struct Bar<B: AsRef<[u8]>>(BytesEncoder<B>);
+
+        delegate! {
+            impl<B: AsRef<[u8]>> Encoder for Bar<B> { 0 }
+        };
+
+        struct Baz<B: AsRef<[u8]>>(BytesEncoder<B>);
+
+        delegate! {
+            impl<B> Encoder for Baz<B> where B: AsRef<[u8]> { 0 }
+        };
+
+        struct BazSized<B: AsRef<[u8]>>(BytesEncoder<B>);
+
+        delegate! {
+            impl<B> Encoder for BazSized<B> where B: AsRef<[u8]>, B: Sized { 0 }
+        };
+    }
+}
